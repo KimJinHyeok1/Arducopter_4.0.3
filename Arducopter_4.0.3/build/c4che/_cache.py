16f7c9f5b2aabe8b37754a054b26c03a161a59d8
smsm772 <smsm772@naver.com>
2021-01-22 10:36:46 +0900

    삭제

--- Arducopter_4.0.3/build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-AUTOCONFIG = True
-BINDIR = '/usr/bin'
-BOARD = 'sitl'
-DEBUG = False
-LIBDIR = '/usr/lib'
-PREFIX = '/usr'
--- Arducopter_4.0.3/build/sitl/libraries/SITL/SIM_RF_uLanding.cpp.0.d
+++
@@ -1,2 +0,0 @@
-libraries/SITL/SIM_RF_uLanding.cpp.0.o: \
- ../../libraries/SITL/SIM_RF_uLanding.cpp ap_config.h
